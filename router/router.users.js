eafc096b5dc6d5980e1894578a455d982a00b05e
heinanX <larrii@live.se>
2023-01-12 11:29:53 +0100

    fixes

--- router/router.users.js
+++ router/router.users.js
@@ -8,6 +8,12 @@ router.get('/', (req, res) => {
     res.status(200).json(users)
 })
 
+router.post('/', (req, res) => {
+    const { username, password} = req.body
+    const user = users.find(element => element.username == username && element.password == password)
+    console.log(user)
+    res.status(200).json(user)
+})
 // router.post('/', (req, res) => {
 //     users.push(req.body)
 //     res.status(201).json(users)
--- users.js
+++ users.js
@@ -1,10 +1,11 @@
 const users = [
-    {"username": "tim", "password": "123", "id": 2},
-    {"username": "jim", "password": "456", "id": 3},
-    {"username": "rin", "password": "789", "id": 4},
-    {"username": "tin", "password": "987", "id": 5},
-    {"username": "sara", "password": "654", "id": 6},
-    {"username": "linda", "password": "321", "id": 7}
+    {username: "lim", password: "012", id: 1},
+    {username: "tim", password: "123", id: 2},
+    {username: "jim", password: "456", id: 3},
+    {username: "rin", password: "789", id: 4},
+    {username: "tin", password: "987", id: 5},
+    {username: "sara", password: "654", id: 6},
+    {username: "linda", password: "321", id: 7}
 ]
 
 module.exports = users
